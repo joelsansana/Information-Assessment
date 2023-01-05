1dde323a68c9f1281a482c4515aedd22d39502cf
Joel Sansana <joel@eq.uc.pt>
2023-01-05 11:56:51 +0000

    Divided script by data characteristics: colinearity, sparsity and nonlinearity

--- data_assessment.py
+++ data_assessment.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 from pylab import rcParams
 from scipy.cluster import hierarchy as hc
 from scipy.special import rel_entr
+from scipy.stats import pearsonr
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import mutual_info_regression
@@ -19,7 +20,42 @@ from sklearn.preprocessing import StandardScaler
 plt.style.use('seaborn-whitegrid')
 rcParams['figure.figsize'] = 15, 8
 
-#%% Get data 1
+import numpy as np
+from sklearn.decomposition import PCA
+
+def parallel_analysis(data, n_iter=1000, n_components=None):
+    # Generate random data with the same number of samples and features
+    random_data = np.random.normal(size=(data.shape[0], data.shape[1]))
+    
+    # Perform PCA on the random data
+    pca = PCA(n_components=n_components)
+    pca.fit(random_data)
+    random_eigenvalues = pca.explained_variance_
+    
+    # Perform PCA on the original data
+    pca = PCA(n_components=n_components)
+    pca.fit(data)
+    original_eigenvalues = pca.explained_variance_
+    
+    # Compute the threshold eigenvalue
+    W = np.zeros(data.shape)
+    pa = np.zeros((n_iter, data.shape[1]))
+    for i in range(n_iter):
+        for j in range(data.shape[1]):
+            W[:, j] = data[np.random.permutation(data.shape[0]), j]
+        pca_pa = PCA().fit(W)
+        pa[i, :] = pca_pa.explained_variance_
+    threshold_eigenvalues = np.percentile(pa, 95, axis=0)
+    
+    # Determine the number of components to retain
+    n_components_to_retain = 0
+    for i in range(len(original_eigenvalues)):
+        if original_eigenvalues[i] > threshold_eigenvalues[i]:
+            n_components_to_retain += 1
+    
+    return original_eigenvalues, threshold_eigenvalues, n_components_to_retain
+
+#%% Get data
 df = pd.read_csv('datasets/dataset1/csv_measurements.csv')
 df = df.iloc[:-1:60, :]
 
@@ -31,7 +67,6 @@ ts1 = t[:-720]
 Xs1 = X.iloc[:-720, :]
 ys1 = y[:-720]
 
-#%% Get data 2
 df = pd.read_csv('datasets/dataset2/csv_measurements.csv')
 df = df.iloc[:-1:60, :]
 
@@ -43,7 +78,6 @@ ts2 = t[:-720]
 Xs2 = X.iloc[:-720, :]
 ys2 = y[:-720]
 
-#%% Get data 3
 df = pd.read_csv('datasets/dataset3/csv_measurements.csv')
 df = df.iloc[:-1:60, :]
 
@@ -55,7 +89,7 @@ ts3 = t[:-720]
 Xs3 = X.iloc[:-720, :]
 ys3 = y[:-720]
 
-#%% Exploratory Data Analysis
+#%% Visualization of key variables
 fig, ax = plt.subplots()
 ax.plot(ts1/(3600*24), ys1, label='1 grade')
 ax.plot(ts2/(3600*24), ys2, label='3 grades')
@@ -89,22 +123,45 @@ plt.xticks(fontsize=16)
 plt.yticks(fontsize=16)
 plt.legend(loc='best')
 
+#%% Colinearity (of X)
+Z1 = StandardScaler().fit_transform(Xs1)
+Z2 = StandardScaler().fit_transform(Xs2)
+Z3 = StandardScaler().fit_transform(Xs3)
+
+eig1, pa1, pc1 = parallel_analysis(Z1, n_iter=100)
+eig2, pa2, pc2 = parallel_analysis(Z2, n_iter=100)
+eig3, pa3, pc3 = parallel_analysis(Z3, n_iter=100)
+
+fig, ax = plt.subplots(nrows=1, ncols=3)
+ax[0].plot(eig1, '-o', label='Eigenvalues')
+ax[1].plot(eig2, '-o', label='Eigenvalues')
+ax[2].plot(eig3, '-o', label='Eigenvalues')
+ax[0].plot(pa1, '-o', label='PA threshold')
+ax[1].plot(pa2, '-o', label='PA threshold')
+ax[2].plot(pa3, '-o', label='PA threshold')
+ax[0].set_title('1 grade', fontsize=18)
+ax[1].set_title('3 grades', fontsize=18)
+ax[2].set_title('DoE', fontsize=18)
+ax[0].legend(loc='upper right', fontsize=16)
+ax[1].legend(loc='upper right', fontsize=16)
+ax[2].legend(loc='upper right', fontsize=16)
+
 C1 = pd.DataFrame(
-    data=np.corrcoef(pd.concat((Xs1, ys1), axis=1), rowvar=False), 
-    columns=[pd.concat((Xs1, ys1), axis=1).columns], 
-    index=np.append(Xs1.columns, 'KPI')
+    data=np.corrcoef(Xs1, rowvar=False), 
+    columns=Xs1.columns, 
+    index=Xs1.columns
     )
 
 C2 = pd.DataFrame(
-    data=np.corrcoef(pd.concat((Xs2, ys2), axis=1), rowvar=False), 
-    columns=[pd.concat((Xs2, ys2), axis=1).columns], 
-    index=np.append(Xs2.columns, 'KPI')
+    data=np.corrcoef(Xs2, rowvar=False), 
+    columns=Xs2.columns, 
+    index=Xs2.columns
     )
 
 C3 = pd.DataFrame(
-    data=np.corrcoef(pd.concat((Xs3, ys3), axis=1), rowvar=False), 
-    columns=[pd.concat((Xs3, ys3), axis=1).columns], 
-    index=np.append(Xs3.columns, 'KPI')
+    data=np.corrcoef(Xs3, rowvar=False), 
+    columns=Xs3.columns, 
+    index=Xs3.columns
     )
 
 fig, ax = plt.subplots(nrows=1, ncols=3)
@@ -115,9 +172,26 @@ ax[1].set_title('3 grades', fontsize=18)
 sns.heatmap(C3, vmin=-1, vmax=1, annot=True, fmt=".2f", cmap="Spectral", ax=ax[2])
 ax[2].set_title('DoE', fontsize=18)
 
+#%% Sparsity of X effects with Y
+c1 = [pearsonr(Xs1[col], ys1)[0] for col in Xs1.columns]
+c2 = [pearsonr(Xs2[col], ys2)[0] for col in Xs2.columns]
+c3 = [pearsonr(Xs3[col], ys3)[0] for col in Xs3.columns]
+
+fig, ax = plt.subplots(nrows=3, ncols=1)
+sns.barplot(x=Xs1.columns, y=c1, ax=ax[0])
+sns.barplot(x=Xs2.columns, y=c2, ax=ax[1])
+sns.barplot(x=Xs3.columns, y=c3, ax=ax[2])
+ax[0].set_ylabel('Corr - 1 grade', fontsize=18)
+ax[1].set_ylabel('Corr - 3 grades', fontsize=18)
+ax[2].set_ylabel('Corr - DoE', fontsize=18)
+
+#%% Nonlinearity
 mi1 = mutual_info_regression(Xs1, ys1)
+mi1 /= mi1.max()
 mi2 = mutual_info_regression(Xs2, ys2)
+mi2 /= mi2.max()
 mi3 = mutual_info_regression(Xs3, ys3)
+mi3 /= mi3.max()
 
 fig, ax = plt.subplots(nrows=3, ncols=1)
 sns.barplot(x=Xs1.columns, y=mi1, ax=ax[0])
@@ -126,54 +200,6 @@ sns.barplot(x=Xs3.columns, y=mi3, ax=ax[2])
 ax[0].set_ylabel('MI - 1 grade', fontsize=18)
 ax[1].set_ylabel('MI - 3 grades', fontsize=18)
 ax[2].set_ylabel('MI - DoE', fontsize=18)
-
-#%% PCA
-Z1 = StandardScaler().fit_transform(Xs1)
-Z2 = StandardScaler().fit_transform(Xs2)
-Z3 = StandardScaler().fit_transform(Xs3)
-
-pca1 = PCA().fit(Z1)
-pca2 = PCA().fit(Z2)
-pca3 = PCA().fit(Z3)
-T1 = pca1.transform(Z1)
-T2 = pca2.transform(Z2)
-T3 = pca3.transform(Z3)
-eig1 = pca1.explained_variance_
-eig2 = pca2.explained_variance_
-eig3 = pca3.explained_variance_
-
-fig, ax = plt.subplots()
-ax.plot(eig1, '-o', label='1 grade')
-ax.plot(eig2, '-o', label='3 grades')
-ax.plot(eig3, '-o', label='DoE')
-ax.axhline(y=1, xmin=0, xmax=5, c='k', ls='--')#%% PCA
-Z1 = StandardScaler().fit_transform(Xs1)
-Z2 = StandardScaler().fit_transform(Xs2)
-Z3 = StandardScaler().fit_transform(Xs3)
-
-pca1 = PCA().fit(Z1)
-pca2 = PCA().fit(Z2)
-pca3 = PCA().fit(Z3)
-T1 = pca1.transform(Z1)
-T2 = pca2.transform(Z2)
-T3 = pca3.transform(Z3)
-eig1 = pca1.explained_variance_
-eig2 = pca2.explained_variance_
-plt.legend(loc='best')
-
-dfT1 = pd.DataFrame(T1[:, :2], columns=['PC1', 'PC2'])
-dfT1['Label'] = ys1.values
-dfT2 = pd.DataFrame(T2[:, :2], columns=['PC1', 'PC2'])
-dfT2['Label'] = ys2.values
-dfT3 = pd.DataFrame(T3[:, :2], columns=['PC1', 'PC2'])
-dfT3['Label'] = ys3.values
-fig, ax = plt.subplots(nrows=1, ncols=3)
-sns.scatterplot(data=dfT1, x='PC1', y='PC2', hue='Label', palette="Spectral", ax=ax[0])
-ax[0].set_title('1 grade', fontsize=18)
-sns.scatterplot(data=dfT2, x='PC1', y='PC2', hue='Label', palette="Spectral", ax=ax[1])
-ax[1].set_title('3 grades', fontsize=18)
-sns.scatterplot(data=dfT3, x='PC1', y='PC2', hue='Label', palette="Spectral", ax=ax[2])
-ax[2].set_title('DoE', fontsize=18)
 
 #%% HC
 fig, ax = plt.subplots(nrows=1, ncols=3)
